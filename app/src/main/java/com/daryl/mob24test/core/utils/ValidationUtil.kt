e392a8c342cd54e7f0f30255c37489b5df4a293f
Daryl <daryl@globalconnect.com>
2024-08-29 14:05:05 +0800

    haha

--- app/src/main/java/com/daryl/mob24test/core/utils/ValidationUtil.kt
+++ app/src/main/java/com/daryl/mob24test/core/utils/ValidationUtil.kt
@@ -2,5 +2,5 @@ package com.daryl.mob24test.core.utils
 
 object ValidationUtil {
     fun validateEmail(email: String): Boolean =
-        Regex("[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+").matches(email)
+        Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.(com)\$").matches(email)
 }
--- app/src/test/java/com/daryl/mob24test/core/utils/ValidationUtilTest.kt
+++ app/src/test/java/com/daryl/mob24test/core/utils/ValidationUtilTest.kt
@@ -9,9 +9,11 @@ class ValidationUtilTest {
     fun `validateEmail should return true for a@adotcom`() {
         assertTrue(ValidationUtil.validateEmail("a@a.com"))
     }
+    @Test
     fun `validateEmail should return false for a@1234`() {
         assertFalse(ValidationUtil.validateEmail("a@1234"))
     }
+    @Test
     fun `validateEmail should return false for a@adotcom$`() {
         assertFalse(ValidationUtil.validateEmail("a@a.com$"))
     }
